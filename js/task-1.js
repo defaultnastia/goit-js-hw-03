69ca99df411b8eabffa2edfb1cff9503d9dfb07a
defaultnastia <anastayaaa@gmail.com>
2024-04-22 18:52:26 +0200

    added hw-3 files

--- js/task-1.js
+++ js/task-1.js
@@ -1,21 +1,12 @@
 'use strict';
 
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalCredits = quantity * pricePerDroid;
-  if (customerCredits < totalCredits) {
-    return 'Insufficient funds!';
-  } else if (customerCredits >= totalCredits) {
-    return `You ordered ${quantity} droids worth ${totalCredits} credits!`;
-  } else {
-    return 'It is over, Anakin. I have the high ground.'; //improvement, error handler
-  }
+function slugify(title) {
+  const slugArray = title.toLowerCase().split(' ');
+  const slugString = slugArray.join('-');
+  return slugString;
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-
-// Error handler, uncomment to check:
-// console.log(makeTransaction(5, 3000, undefined));
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-3.js
+++ js/task-3.js
@@ -1,23 +1,18 @@
 'use strict';
 
-function checkForSpam(message) {
-  if (typeof message != 'string') {
-    return 'The ability to speak does not make you intelligent.'; //improvement, error handler
+function filterArray(numbers, value) {
+  const arrayOfBigValues = [];
+  for (const numberElement of numbers) {
+    if (numberElement <= value) {
+      continue;
+    }
+    arrayOfBigValues.push(numberElement);
   }
-
-  return message.toLowerCase().includes('spam') ||
-    message.toLowerCase().includes('sale')
-    ? true
-    : false;
+  return arrayOfBigValues;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
-
-// Error handler, uncomment to check:
-// console.log(checkForSpam(null));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
